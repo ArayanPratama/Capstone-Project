c01b0924714da123510ce029a41d57633cd65491
Arrayyan-Pratama <arayanpratama265@gmail.com>
2024-06-14 14:10:01 +0700

    update

---
+++ assets/penyimpanan js.js
@@ -0,0 +1,13 @@
+ // var name = prompt ("What is your name");
+      // var hurufPertama = name.slice(0,1);
+      // var kapitalHurufPertama = hurufPertama.toUpperCase();
+      // var sisaNama = name.slice(1, name.length);
+      // var kapital = kapitalHurufPertama + sisaNama;
+      // alert ("Hello " +kapital );
+      
+      
+      // function  myFunction(){
+    //   var umurKucing = prompt ("Berapa umur kucing kamu? ");
+    //   var umurManusia =  ((umurKucing - 2)*4+21);
+    //   alert("Umur kucing kamu setara dengan: " +umurManusia+ " Tahun umur manusia");
+    // } 
